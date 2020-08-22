2fffd0babf47dd9cd1add7accd7f721d8ad6e217
Teebreezy101 <tammy.batubo@gmail.com>
2020-08-22 15:40:37 +0100

    add swiper and price section

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -114,3 +114,15 @@ document.addEventListener('DOMContentLoaded', function(){
   }, false);
 
 
+// Swiper
+var swiper = new Swiper('.swiper-container', {
+    effect: 'flip',
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
